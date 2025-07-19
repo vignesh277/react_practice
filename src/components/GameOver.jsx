6f30332dc8ecc03798b28270db5753dbe22ca2bd
vignesh277 <yenagantivignesh@gmail.com>
2025-07-19 21:59:39 +0530

    tic tac project

---
+++ src/components/GameOver.jsx
@@ -0,0 +1,13 @@
+export default function GameOver({winner,onRestart}){
+    return(
+
+        <div id="game-over">
+            <h2>Game over!</h2>
+           { winner&& <p>{winner} won!</p>}
+           {!winner&&<p>its a draw</p>}
+            <p>
+                <button onClick={onRestart}>Rematch</button>
+            </p>
+        </div>
+    );
+}
